980f317ba8762c425e70b92c17e838101fb09216
spaghettipababa <omaguingerich98@gmail.com>
2024-10-04 11:38:01 +0800

    ErichOmaguing.py

---
+++ Activity 7/ErichOmaguing.py
@@ -0,0 +1,30 @@
+IT_years_of_service = float(input("Years of service in the IT Department: "))
+
+if IT_years_of_service >= 10:
+    print("IT Bonus will be: 10000")
+elif IT_years_of_service <10 and IT_years_of_service >1:
+    print("IT Bonus will be: 5000")
+else:
+    print("No Bonus, Bawi Nexlayp :) ")
+    
+ACCT_years_of_service = float(input("Years of service in the Accounting Department: "))
+
+if ACCT_years_of_service >= 10:
+    print("Accounting Bonus will be: 12000")
+elif ACCT_years_of_service <10 and ACCT_years_of_service >1:
+    print("Accounting Bonus will be: 6000")
+else:
+    print("No Bonus, Bawi Nexlayp :) ")
+    
+HR_years_of_service = float(input("Years of service in the Human Resource Department: "))
+
+if HR_years_of_service >= 10:
+    print("HR Bonus will be: 15000")
+elif HR_years_of_service <10 and HR_years_of_service >1:
+    print("HR Bonus will be: 7500")
+else:
+    print("No Bonus, Bawi Nexlayp :) ")
+    
+    
+    
+
