e12a50d06ea4d195ed916f6e04d821f8d8da0a34
spaghettipababa <omaguingerich98@gmail.com>
2024-10-04 11:21:17 +0800

    ErichOmaguing.py

---
+++ Activity 2/ErichOmaguing.py
@@ -0,0 +1,7 @@
+Math = int(input("Grade in Mathematics: "))
+English = int(input("Grade in English: "))
+Science = int(input("Grade in Science: "))
+
+sum = (Math + English + Science)
+average = (sum / 3)
+print("average: " + str(average))
